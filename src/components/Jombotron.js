37445aa8809901b4cff05b8a2c88308093e81c06
sundarkumarsatyala <sundarkumarsatyala@gmail.com>
2021-01-30 14:51:16 +0530

    add Jumbotron

---
+++ src/components/Jombotron.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Container,
+    Row,
+    Col,
+    Jumbotron,
+    Button
+} from 'reactstrap';
+
+class Jombotron extends Component {
+    constructor(props) {
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            isOpen: false
+        };
+    }
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+    render() {
+        return (
+            <div>
+                <Jumbotron>
+                    <Container>
+                        <Row>
+                            <Col>
+                                <h1>Welcome to ReactJS</h1>
+                                <p>
+                                    <Button
+                                        tag="a"
+                                        color="success"
+                                        size="large"
+                                        href="http://reactstrap.github.io"
+                                        target="_blank"
+                                    >
+                                        View Reactstrap Docs
+                                    </Button>
+                            </Col>
+                        </Row>
+                    </Container>
+                </Jumbotron>
+            </div>
+        );
+    }
+}
+
+export default Jombotron;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Navigationbar from "./components/Navigationbar";
+import Jumbotron from "./components/Jumbotron";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <Navigationbar />
+    <Jumbotron />
   </React.StrictMode>,
   document.getElementById('root')
 );
